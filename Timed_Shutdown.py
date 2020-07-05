5b45b88edecb61129a7bfb5875f5d6369e2ee62a
Concrete18 <michaelericson19@gmail.com>
2020-07-05 18:42:20 -0400

    small changes

--- Timed_Shutdown.py
+++ Timed_Shutdown.py
@@ -1,3 +1,4 @@
+import PySimpleGUIWx as sg
 import datetime as dt
 import configparser
 import subprocess
@@ -7,65 +8,72 @@ import os
 
 Config = configparser.ConfigParser()
 Config.read('Config.ini')
-default_sleep_standby = Config.get('Settings', 'default_sleep_standby')
-check_standby = Config.get('Settings', 'check_standby')
+sg.theme('DarkTanBlue')   # Add a touch of color
+# All the stuff inside your window.
+layout = [  [sg.Radio('Sleep', "SLEEP", default=True),sg.Radio('Shutdown', "SHUTDOWN")],
+            [sg.Text('How many minutes before action starts?'), sg.InputText()],
+            [sg.Button('Start'), sg.Button('Cancel')]]
+
+class Timer:
+    def __init__(self):
+        self.default_sleep_standby = Config.get('Settings', 'default_sleep_standby')
+        self.check_standby = Config.get('Settings', 'check_standby')
+        self.min_left = 'UNSET'
+        self.actiom = ''
+        self.delay = ''
 
 
-def get_standby_time():
-    # Gets Active Power scheme
-    current_scheme = str(subprocess.check_output([f"powercfg", "/getactivescheme"])).split(' ')[3]
-    # Gets Current Scheme Sleep Standby time.
-    output = str(subprocess.check_output([f"powercfg", "/q", current_scheme, "SUB_SLEEP"]))
-    output.split(' ')[1][:-4]
-    string = 'Current AC Power Setting Index:'
-    cur_standby_time = output.partition(str(string))[2].split(' ')[1][:-4]
-    return int(int(cur_standby_time, 16) / 60)
+
+    @staticmethod
+    def get_standby_time():
+        # Gets Active Power scheme
+        current_scheme = str(subprocess.check_output([f"powercfg", "/getactivescheme"])).split(' ')[3]
+        # Gets Current Scheme Sleep Standby time.
+        output = str(subprocess.check_output([f"powercfg", "/q", current_scheme, "SUB_SLEEP"]))
+        output.split(' ')[1][:-4]
+        string = 'Current AC Power Setting Index:'
+        cur_standby_time = output.partition(str(string))[2].split(' ')[1][:-4]
+        return int(int(cur_standby_time, 16) / 60)
 
 
-def Timed_shutdown_sleep(use_config, config_standby):
-    if use_config == 1:
-        def_standby = get_standby_time()
-    else:
-        def_standby = config_standby
-    subprocess.call(f"powercfg -change -standby-timeout-ac {def_standby}")
-    print('Shutdown/Sleep Timer\n')
-    last_run = dt.datetime.now()
-    power = {1:'sleep', 2:'shutdown'}
-    action = int(input('What do you want to do?\n1-Sleep  2-Shutdown\n'))
-    response = float(input(f'\nHow long till you want your PC to {power[action]} in minutes.\n'))
-    timer = response * 60
-    print('')
-    if response > def_standby:
-        subprocess.call(f"powercfg -change -standby-timeout-ac {timer + 5}")
-        print(f'Default Standby Time is {def_standby} minutes.\n\
-            Standy Timer was changed so do not close manually so it goes back to default.\n')
-    while timer > 0:
-        if dt.datetime.now() - last_run >= dt.timedelta(minutes=1.1):
-            response = input('Sleep Detected: Press Enter to close or type r to restart.')
-            if response == 'r':
-                Timed_shutdown_sleep()
-            else:
-                subprocess.call(f"powercfg -change -standby-timeout-ac {def_standby}")
-                sys.exit()
-        min_left = int(timer / 60)
-        if min_left == 1:
-            min_plur = 'Minute'
+    def Timed_shutdown_sleep(self):
+        if self.use_config == 1:
+            self.def_standby = get_standby_time()
         else:
-            min_plur = 'Minutes'
-        print(f'System will {power[action]} in {min_left} {min_plur} and {int(timer % 60)} Seconds.        ', end='\r')
-        time.sleep(1)
-        timer -= 1
+            self.def_standby = self.default_sleep_standby
+        subprocess.call(f"powercfg -change -standby-timeout-ac {self.def_standby}")
         last_run = dt.datetime.now()
-    print('Timer Finished')
-    subprocess.call(f"powercfg -change -standby-timeout-ac {def_standby}")
-    print(f'\n{power[action].capitalize()} Initiated')
-    if action == 1: # Sleep
-        os.system("rundll32.exe powrprof.dll,SetSuspendState 0,1,0")
-        time.sleep(10)
-        sys.exit()
-    elif action == 2: # Shutdown
-        os.system("shutdown /s /t 1")
-    input()
+        timer = delay * 60
+        if delay > self.def_standby:
+            subprocess.call(f"powercfg -change -standby-timeout-ac {timer + 5}")
+        while timer > 0:
+            if dt.datetime.now() - last_run >= dt.timedelta(minutes=1.1):
+                subprocess.call(f"powercfg -change -standby-timeout-ac {self.def_standby}")
+                sys.exit()
+            min_left = int(timer / 60)
+            if min_left == 1:
+                min_plur = 'Minute'
+            else:
+                min_plur = 'Minutes'
+            time.sleep(1)
+            timer -= 1
+            last_run = dt.datetime.now()
+        subprocess.call(f"powercfg -change -standby-timeout-ac {self.def_standby}")
+        if self.action == 1: # Sleep
+            os.system("rundll32.exe powrprof.dll,SetSuspendState 0,1,0")
+            time.sleep(10)
+            sys.exit()
+        elif self.action == 2: # Shutdown
+            os.system("shutdown /s /t 1")
 
 if __name__ == '__main__':
-    Timed_shutdown_sleep(check_standby, default_sleep_standby)
+    App = Timer()
+    window = sg.Window('Timed Shutdown and Sleep', layout)
+    while True:
+        event, values = window.read()
+        if event == sg.WIN_CLOSED or event == 'Cancel': # if user closes window or clicks cancel
+            break
+        if event == sg.WIN_CLOSED or event == 'Start':
+            self.Timed_shutdown_sleep(values[0])
+
+    window.close()
