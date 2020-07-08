86ee6cfb45064386d156e8a0e76ea2b40e28b192
Concrete18 <michaelericson19@gmail.com>
2020-07-08 00:19:38 -0400

    Finished cancel and timer interface.

--- Timed_Shutdown.py
+++ Timed_Shutdown.py
@@ -1,10 +1,9 @@
-import tkinter as Tk
 from functools import partial
-from tkinter import ttk
 import datetime as dt
+import tkinter as Tk
 import configparser
 import subprocess
-from functools import partial
+import threading
 import time
 import sys
 import os
@@ -15,7 +14,6 @@ Config.read('Config.ini')
 def create_window():
     default_sleep_standby = Config.get('Settings', 'default_sleep_standby')
     check_standby = Config.get('Settings', 'check_standby')
-    min_left = 'UNSET'
 
 
     def get_standby_time():
@@ -29,39 +27,57 @@ def create_window():
         return int(int(cur_standby_time, 16) / 60)
 
 
-    def Timed_shutdown_sleep(delay, action):
+    def Timed_shutdown_sleep(action):
         if check_standby == 1:
             def_standby = get_standby_time()
         else:
             def_standby = default_sleep_standby
         subprocess.call(f"powercfg -change -standby-timeout-ac {def_standby}")
         last_run = dt.datetime.now()
-        print(f'Delay is set to "{delay}"')
-        print(f'Action is set to "{action}"')
+        delay = Timer_Entry.get()
+        Sleep_Button.config(state='disabled')
+        Shutdown_Button.config(state='disabled')
+        Cancel_Button.config(state='normal')
         timer = int(delay) * 60
         if delay > def_standby:
             subprocess.call(f"powercfg -change -standby-timeout-ac {timer + 5}")
+        thread = threading.Thread(target=Time_Loop, args=(timer, action, last_run, def_standby), daemon=True)
+        thread.start()
+
+
+    def Time_Loop(timer, action, last_run, def_standby):
         while timer > 0:
             if dt.datetime.now() - last_run >= dt.timedelta(minutes=1.1):
                 subprocess.call(f"powercfg -change -standby-timeout-ac {def_standby}")
                 sys.exit()
             min_left = int(timer / 60)
+            sec_left = "{0:0=2d}".format(int(timer % 60))
             if min_left == 1:
                 min_plur = 'Minute'
             else:
                 min_plur = 'Minutes'
+            if min_left >= 1:
+                Timer_Display.config(text=f'Time Left: {min_left}:{sec_left}')
+            else:
+                Timer_Display.config(text=f'Time Left: 0 minutes and {sec_left} seconds')
             time.sleep(1)
             timer -= 1
             last_run = dt.datetime.now()
         subprocess.call(f"powercfg -change -standby-timeout-ac {def_standby}")
-        if action == 1: # Sleep
-            print('Sleep')
+        if action == 'Sleep': # Sleep
+            print(action)
             # os.system("rundll32.exe powrprof.dll,SetSuspendState 0,1,0")
             # time.sleep(10)
             # sys.exit()
-        elif action == 2: # Shutdown
-            print('Shutdown')
+        elif action == 'Shutdown': # Shutdown
+            print(action)
             # os.system("shutdown /s /t 1")
+        Main_GUI.destroy()
+
+
+    def Cancel_Func():
+        # subprocess.call(f"powercfg -change -standby-timeout-ac {def_standby}")
+        sys.exit()
 
 
     # Defaults for Background and fonts
@@ -70,13 +86,13 @@ def create_window():
     BaseFont = "Arial"
     FontColor = "Black"
 
-    Timed_GUI = Tk.Tk()
-    Timed_GUI.title("Timed Shutdown and Sleep")
-    Timed_GUI.iconbitmap('Power.ico')
-    Timed_GUI.configure(bg=Background)
-    Timed_GUI.resizable(width=False, height=False)
+    Main_GUI = Tk.Tk()
+    Main_GUI.title("Timed Shutdown and Sleep")
+    Main_GUI.iconbitmap('Power.ico')
+    Main_GUI.configure(bg=Background)
+    Main_GUI.resizable(width=False, height=False)
 
-    Title_Frame = Tk.Frame(Timed_GUI, bg=Background)
+    Title_Frame = Tk.Frame(Main_GUI, bg=Background)
     Title_Frame.grid(columnspan=4, padx=(20, 20), pady=(5, 10))
 
     Title = Tk.Label(Title_Frame, text='Timed Shutdown and Sleep', font=(BoldBaseFont, 20), bg=Background)
@@ -85,16 +101,25 @@ def create_window():
     Question = Tk.Label(Title_Frame, text='Enter Time in minutes before action starts.', font=(BoldBaseFont, 12), bg=Background)
     Question.grid(columnspan=4, row=1)
 
-    Timer_Entry = Tk.Entry(Timed_GUI, width=10, bd=2, font=(BaseFont, 13), justify='center', bg='grey95')
+    Timer_Entry = Tk.Entry(Main_GUI, width=10, bd=2, font=(BaseFont, 13), justify='center', bg='grey95')
     Timer_Entry.grid(columnspan=4, row=2)
 
-    Sleep_Button = Tk.Button(Timed_GUI, text=f'Sleep Computer', font=(BaseFont, 11), width=20, command=partial(Timed_shutdown_sleep, Timer_Entry.get(), 1))
+    Sleep_Button = Tk.Button(Main_GUI, text=f'Sleep Computer', font=(BaseFont, 11), width=20, command=partial(Timed_shutdown_sleep, 'Sleep'))
     Sleep_Button.grid(column=1, row=3, pady=(10, 10))
 
-    Shutdown_Button = Tk.Button(Timed_GUI, text=f'Shutdown Computer', font=(BaseFont, 11), width=20, command=partial(Timed_shutdown_sleep, Timer_Entry.get(), 2))
+    Shutdown_Button = Tk.Button(Main_GUI, text=f'Shutdown Computer', font=(BaseFont, 11), width=20, command=partial(Timed_shutdown_sleep, 'Shutdown'))
     Shutdown_Button.grid(column=2, row=3, pady=(10, 10))
 
-    Timed_GUI.mainloop()
+    Timer_Frame = Tk.Frame(Main_GUI, bg=Background)
+    Timer_Frame.grid(columnspan=4, row=4, padx=(20, 20), pady=(5, 10))
+
+    Timer_Display = Tk.Label(Timer_Frame, text='Time Left: Waiting to start', font=(BoldBaseFont, 12), bg=Background)
+    Timer_Display.grid(columnspan=4, row=0)
+
+    Cancel_Button = Tk.Button(Timer_Frame, text=f'Cancel Action', font=(BaseFont, 11), width=20, command=Cancel_Func, state='disabled')
+    Cancel_Button.grid(columnspan=4, row=1, pady=(10, 1))
+
+    Main_GUI.mainloop()
 
 if __name__ == '__main__':
     create_window()
